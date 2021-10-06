aeec998e58aa6e239314c84b720f8c2fb9528f16
Adam Kopuletý <zerostar362@live.com>
2021-10-07 01:08:02 +0200

    version

--- SocerTrackerProject/VersionInfo/VersionInfo.cs
+++ SocerTrackerProject/VersionInfo/VersionInfo.cs
@@ -8,6 +8,6 @@ namespace SocerTrackerProject.VersionInfo
 {
     class VersionInfo
     {
-        string Version = "v0.0.1alpha ";
+        string Version = "v0.0.1alpha:8b9b9d3e9ad3499baddfef0c1b5236fb68e7b835 ";
     }
 }
